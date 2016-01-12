867b3898bd55040f9d59e5783a69fc8eee10f9f9
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-12 17:38:12 -0600

    Add words when store is created

--- app/services/hangman-store.js
+++ app/services/hangman-store.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import Redux from 'npm:redux';
+import hangmanReducer from '../reducers/hangman';
+import dictionary from '../utils/dictionary';
+
+let {set} = Ember;
+let {createStore} = Redux;
 
 export default Ember.Service.extend({
+  init() {
+    this._super(...arguments);
+
+    set(this, 'store', createStore(hangmanReducer));
+
+    this.addWords();
+  },
+
+  addWords() {
+    dictionary.forEach((word) => {
+      this.get('store').dispatch({type: 'ADD_WORD', word});
+    });
+  },
 });
---
+++ app/utils/dictionary.js
@@ -0,0 +1,8 @@
+export default [
+  'category',
+  'football',
+  'ember',
+  'daniel',
+  'mary',
+  'ryan',
+];
