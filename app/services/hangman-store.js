6ce51e7df40db751905b40eed05e0d745d194a31
Ryan Tablada <ryan.tablada@gmail.com>
2016-01-12 17:51:47 -0600

    Starting dictionary

--- app/services/hangman-store.js
+++ app/services/hangman-store.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Redux from 'npm:redux';
 import hangmanReducer from '../reducers/hangman';
 import dictionary from '../utils/dictionary';
+import _ from 'lodash';
 
 let {set} = Ember;
 let {createStore} = Redux;
@@ -12,12 +13,37 @@ export default Ember.Service.extend({
 
     set(this, 'store', createStore(hangmanReducer));
 
+    this.pushState();
     this.addWords();
   },
 
   addWords() {
     dictionary.forEach((word) => {
       this.get('store').dispatch({type: 'ADD_WORD', word});
+    });
+  },
+
+  pushState() {
+    let store = this.get('store');
+
+    store.subscribe(() => {
+      this.set('state', store.getState());
+    });
+  },
+
+  randomWord() {
+    const index = _.random(this.get('state.wordList.length') - 1);
+
+    this.store.dispatch({
+      type: 'CHOOSE_WORD',
+      word: this.get('state.wordList')[index],
+    });
+  },
+
+  sendAction(type, data) {
+    this.store.dispatch({
+      ...data,
+      type,
     });
   },
 });
